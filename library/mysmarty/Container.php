2459645a4dd9f11b63a5ff8575b768fa23641ef9
全球变暖 <1445023846@qq.com>
2021-08-28 11:31:44 +0800

    单一实例

---
+++ library/mysmarty/Container.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace library\mysmarty;
+
+/**
+ * 单一实例
+ */
+class Container
+{
+    // 保存单一实例变量的数组
+    private static array $_instance = [];
+    // 除了配置的变量，其它的都重新初始化
+    protected array $flushExceptVar = [];
+    // 重新初始化配置的变量
+    protected array $flushOnlyVar = [];
+
+    /**
+     * 禁止实例化
+     */
+    final private function __construct()
+    {
+    }
+
+    /**
+     * 禁止克隆
+     */
+    final protected function __clone()
+    {
+    }
+
+    /**
+     * 获取单一实例
+     * @return static
+     */
+    final public static function getInstance(): static
+    {
+        $class = get_called_class();
+        if (false === array_key_exists($class, static::$_instance)) {
+            static::$_instance[$class] = new static;
+            static::$_instance[$class]->_initialize();
+        }
+        return static::$_instance[$class];
+    }
+
+    /**
+     * 实例化后，调用初始化
+     */
+    protected function _initialize()
+    {
+    }
+
+    /**
+     * 重新初始化变量的值
+     */
+    protected function _flush()
+    {
+    }
+}
